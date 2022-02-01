6de00fdbe3be8353b29e51849a1e9d632855f991
Antonio <ditacj78@gmail.com>
2022-02-01 22:10:30 +0100

    tailwind.config.js: added colors, custom properties

--- web-replica/tailwind.config.js
+++ web-replica/tailwind.config.js
@@ -3,14 +3,32 @@ module.exports = {
     darkMode: false, // or 'media' or 'class'
     theme: {
       extend: {
-          backgroundImage: {
-              'pattern': "url('/img/pattern.png')",
-              'footer-texture': "url('/img/footer-texture.png')",
+          backgroundImage: theme => ({
+              'pattern': "url('./img/pattern.png')"
+          }),
+          fontFamily: theme =>({
+            symbols: ['WebSymbolsRegular'],
+            bebas: ['BebasNeue'],
+          }),
+          colors: theme => ({
+            'azul-logo':'#37bcf9',
+            'hover-logo':'#ccc',
+          }),
+          height: {
+            'header':'70px',
+          },
+          width: {
+            'wrapper':'1250px',
+          },
+          margin: {
+            'wrapper':'0px auto',
           }
       },
     },
     variants: {
-      extend: {},
+      extend: {
+        
+      },
     },
     plugins: [],
 }
