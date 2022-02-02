e0dc905a70b804c51bf03d18cc2bddeb2ab7827f
Antonio Cortés <ditacj78@gmail.com>
2022-02-02 20:13:17 +0100

    web-replica: added several changes

--- web-replica/tailwind.config.js
+++ web-replica/tailwind.config.js
@@ -1,19 +1,43 @@
 module.exports = {
-    purge: [],
-    darkMode: false, // or 'media' or 'class'
     theme: {
       extend: {
+          keyframes: {
+            fromBellow: {
+              '0%, 100%': { transform: 'translate(0%)'},
+              '50%': { transform: 'translate(200%)'},
+            },
+            showIcon: {
+              'from': { transform: 'translate(-200%)' },
+              'to': { transform: 'translate(0%)' }
+            },
+            showCategory: {
+              'from': { transform: 'translate(-300%)', color: 'black' },
+              'to': { transform: 'translate(0%)', color: 'white' }
+            },
+            showDescription: {
+              'from': { transform: 'translate(-300%)', color: 'black' },
+              'to': { transform: 'translate(0%)' }
+            }
+          },
+          animation: {
+            'rotateGear': 'spin 2s linear infinite' ,
+            'showIconCard':'showIcon 300ms linear',
+            'showCategoryCard':'showCategory 400ms linear',
+            'showDescriptionCard':'showDescription 400ms linear'
+          },
           backgroundImage: theme => ({
-              'pattern': "url('./img/pattern.png')"
+              'pattern': "url('../img/pattern.png')",
+              'banner': "url(../img/bakbaner.png)"
           }),
-          fontFamily: theme =>({
+          fontFamily: {
             symbols: ['WebSymbolsRegular'],
             bebas: ['BebasNeue'],
-          }),
-          colors: theme => ({
+          },
+          colors: {
             'azul-logo':'#37bcf9',
+            'azul-category': '00538c',
             'hover-logo':'#ccc',
-          }),
+          },
           height: {
             'header':'70px',
           },
@@ -22,13 +46,16 @@ module.exports = {
           },
           margin: {
             'wrapper':'0px auto',
+          },
+          lineHeight: {
+            'menu': '70px',
+            'icon': '110px'
           }
       },
     },
     variants: {
-      extend: {
-        
-      },
-    },
-    plugins: [],
+      animation: ['responsive', 'hover', 'group-hover'],
+      animate: ['responsive', 'hover', 'group-hover'],
+      fontSize: ['responsive', 'hover', 'group-hover'],
+    }
 }
