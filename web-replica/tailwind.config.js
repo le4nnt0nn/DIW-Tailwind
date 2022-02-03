d12fc545be26c832f2a1dd619cd8792ae1cc7ab5
Antonio <ditacj78@gmail.com>
2022-02-03 15:20:45 +0100

    added banner animation

--- web-replica/tailwind.config.js
+++ web-replica/tailwind.config.js
@@ -17,13 +17,25 @@ module.exports = {
             showDescription: {
               'from': { transform: 'translate(-300%)', color: 'black' },
               'to': { transform: 'translate(0%)' }
+            },
+            backBanner: {
+              '0%': { backgroundPosition: '0px 0px' },
+              '100%': {backgroundPosition: '-200px -200px' }
+            },
+            showText: {
+              '0%': { transform: 'translate(-600%) scale(5,5)' },
+              '50%': { transform: 'translate(600%) scale(5,5)', opacity: 1 },
+              '75%': { transform: 'translate(-600%)', dropShadow: 'none', color: 'transparent', opacity: 0 },
+              '100%': { transform: 'translate(0%) scale(1,1)', dropShadow: 'drop-shadow-md', color: 'white', opacity: 1 }
             }
           },
           animation: {
             'rotateGear': 'spin 2s linear infinite' ,
             'showIconCard':'showIcon 300ms linear',
             'showCategoryCard':'showCategory 400ms linear',
-            'showDescriptionCard':'showDescription 400ms linear'
+            'showDescriptionCard':'showDescription 400ms linear',
+            'backBannerBan':'backBanner 10s linear',
+            'showTextBan':'showText 10s linear'
           },
           backgroundImage: theme => ({
               'pattern': "url('../img/pattern.png')",
