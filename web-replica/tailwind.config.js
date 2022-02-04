29d8eb34215a7ac1526ff5e171f025a556d73089
Antonio <ditacj78@gmail.com>
2022-02-04 22:04:02 +0100

    added all styles and structure (not finished)

--- web-replica/tailwind.config.js
+++ web-replica/tailwind.config.js
@@ -3,8 +3,12 @@ module.exports = {
       extend: {
           keyframes: {
             fromBellow: {
-              '0%, 100%': { transform: 'translate(0%)'},
-              '50%': { transform: 'translate(200%)'},
+              '0%, 100%': { transform: 'translateY(0%)'},
+              '50%': { transform: 'translateY(200%)'},
+            },
+            fromRight: {
+              'from': { transform: 'translateX(200%)' },
+              'to': { transform: 'translateX(0%)' }
             },
             showIcon: {
               'from': { transform: 'translateY(-200%)' },
@@ -27,15 +31,25 @@ module.exports = {
               '50%': { transform: 'translate(600%) scale(5,5)', opacity: 1 },
               '75%': { transform: 'translate(-600%)', dropShadow: 'none', color: 'transparent', opacity: 0 },
               '100%': { transform: 'translate(0%) scale(1,1)', dropShadow: 'drop-shadow-md', color: 'white', opacity: 1 }
+            },
+            blink: {
+              '0%': { border: '10px solid #37bcf9' },
+              '25%': { border: '10px solid green' },
+              '50%': { border: '10px solid yellow' },
+              '75%': { border: '10px solid white' },
+              '100%': { border: '10px solid #37bcf9' }
             }
           },
           animation: {
+            'fromBellowLogo':'fromBellow 500ms linear',
+            'fromRightLogo':'fromRight 300ms linear',
             'rotateGear': 'spin 2s linear infinite' ,
             'showIconCard':'showIcon 300ms linear',
             'showCategoryCard':'showCategory 400ms linear',
             'showDescriptionCard':'showDescription 400ms linear',
             'backBannerBan':'backBanner 10s linear',
-            'showTextBan':'showText 10s linear'
+            'showTextBan':'showText 10s linear',
+            'blinkBlog':'blink 10s infinite linear'
           },
           backgroundImage: theme => ({
               'pattern': "url('../img/pattern.png')",
@@ -53,6 +67,7 @@ module.exports = {
           },
           height: {
             'header':'70px',
+            'footerWrap':'370px'
           },
           width: {
             'wrapper':'1250px',
@@ -64,7 +79,8 @@ module.exports = {
           lineHeight: {
             'menu': '70px',
             'icon': '110px',
-            'h3': '45px'
+            'h3': '45px',
+            'blog':'100px'
           },
           MinHeight: {
             'lateral': '1200px',
